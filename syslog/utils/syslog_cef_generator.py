d1358fff8ebaaed8b449937090512fb0a9e29a15
kkawana07 <105899219+kkawana07@users.noreply.github.com>
2022-06-28 13:59:59 +0900

    Update syslog_cef_generator.py

--- syslog/utils/syslog_cef_generator.py
+++ syslog/utils/syslog_cef_generator.py
@@ -298,7 +298,7 @@ class CEFGenerator(object):
                 extension_strs["rt"] = date
 
         extensions_str = ",".join(
-            reversed("{}={}".format(k, v) for k, v in extension_strs.items())
+            sorted("{}={}".format(k, v) for k, v in extension_strs.items())
         )
 
         # Append extension string
